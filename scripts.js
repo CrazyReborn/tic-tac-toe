643e1440e2151a6d5e32f005fd2855a0d0e978dc
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2021-09-13 20:27:44 +0200

    Added a button to reset the game. The button shows only when the game is over and resets the game.

--- scripts.js
+++ scripts.js
@@ -24,6 +24,7 @@ const renderBoard = (() => {
         document.querySelector('.gameboard').innerHTML = '';
     };
     newBoard();
+
     return {newBoard, clearBoard};
 })();
 
@@ -31,29 +32,63 @@ const playerFactory = (name, key) => {
     return {name, key};
 };
 
-let activePlayer = 'X';
-
 const gameController = (() => {
+    const playerOne = playerFactory('name', 'X');
+    const playerTwo = playerFactory('name', 'O');
+
+    let winStatus = false;
+    
+    const buttonReset = document.querySelector('button');
+    buttonReset.addEventListener('click', e => {
+        gameBoard.clear();
+        renderBoard.clearBoard();
+        renderBoard.newBoard();
+        document.querySelector('#display').firstElementChild.textContent = '';
+        playerOne.name = document.querySelector('#player-one-name').value = '';
+        playerTwo.name = document.querySelector('#player-two-name').value = '';
+        addListeners();
+        e.target.style.display = 'none';
+    });
+
+    document.querySelector('#submit-button').addEventListener('click', e => {
+        playerOne.name = document.querySelector('#player-one-name').value;
+        playerTwo.name = document.querySelector('#player-two-name').value;
+        document.querySelector('#display').firstElementChild.textContent = `It's now ${activePlayer.name}'s turn.'`
+        e.preventDefault();
+    });
+
+    function updateDisplay() {
+        document.querySelector('#display').firstElementChild.textContent = `It's now ${activePlayer.name}'s turn.'`
+    };
+       
+    let activePlayer = playerOne;
     function turn() {
-        if (activePlayer == 'X') {
-            activePlayer = 'O';
+        if (activePlayer == playerOne) {
+            activePlayer = playerTwo;
         }
         else {
-            activePlayer = 'X';
+            activePlayer = playerOne;
         }
     };
     function checkWin() {
-        const winCombo = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [0,4,6]]
-        for (let i = 0; i < 7; i++) {
+        const winCombo = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+        for (let i = 0; i < 8; i++) {
             const combo = winCombo[i];
             let one = gameBoard.gameBoardContent[combo[0]];
             let two = gameBoard.gameBoardContent[combo[1]];
             let three = gameBoard.gameBoardContent[combo[2]];
             if ((one === '') || (two === '') || (three === '')) {
                 continue;
-            }
+            } 
             else if (one == two && one == three) {
-                alert('WIN!');
+                document.querySelector('#display').firstElementChild.textContent = `${activePlayer.name} WINS!`;
+                winStatus = true;
+                document.querySelector('button').style.display = 'inline-block';
+            }
+            else if (!gameBoard.gameBoardContent.includes('') && (one != two && one != three)) {
+                document.querySelector('#display').firstElementChild.textContent =`IT's A TIE BETWEEN ${playerOne.name} and ${playerTwo.name}!`;
+                winStatus = true;
+                document.querySelector('button').style.display = 'inline-block';
                 break;
             }
         }
@@ -61,18 +96,21 @@ const gameController = (() => {
     function addListeners() {
         document.querySelectorAll('#cell').forEach(e => {
             e.addEventListener('click', function() {
-                if (activePlayer && !e.getAttribute('data-changed', true)) {
-                    e.firstElementChild.textContent = activePlayer;
+                if (activePlayer.key && !e.getAttribute('data-changed', true)) {
+                    e.firstElementChild.textContent = activePlayer.key;
                     e.setAttribute('data-changed', true);
                     gameBoard.gameBoardContent.splice(e.getAttribute('data-element'), 1, e.firstElementChild.textContent);
                 } else if (e.getAttribute('data-changed', true)) {
                     alert("STOP! YOU VIOLATED THE LAW!");
                 }
                 checkWin();
-                turn();
+                if (!winStatus) {
+                    turn();
+                    updateDisplay();
+                }
                 });
             });
     };
     addListeners();
-    return {checkWin, addListeners,turn, playerOne, playerTwo};
+    return {checkWin, addListeners,turn};
 })();

449bf456fb2811a813eee3b9fe4d1081ddf3b9b1
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2021-09-14 20:32:37 +0200

    The game now starts only when a start button is pushed.

--- scripts.js
+++ scripts.js
@@ -23,6 +23,7 @@ const renderBoard = (() => {
     function clearBoard() {
         document.querySelector('.gameboard').innerHTML = '';
     };
+
     newBoard();
 
     return {newBoard, clearBoard};
@@ -37,80 +38,85 @@ const gameController = (() => {
     const playerTwo = playerFactory('name', 'O');
 
     let winStatus = false;
-    
-    const buttonReset = document.querySelector('button');
-    buttonReset.addEventListener('click', e => {
-        gameBoard.clear();
-        renderBoard.clearBoard();
-        renderBoard.newBoard();
-        document.querySelector('#display').firstElementChild.textContent = '';
-        playerOne.name = document.querySelector('#player-one-name').value = '';
-        playerTwo.name = document.querySelector('#player-two-name').value = '';
-        addListeners();
-        e.target.style.display = 'none';
-    });
 
     document.querySelector('#submit-button').addEventListener('click', e => {
         playerOne.name = document.querySelector('#player-one-name').value;
         playerTwo.name = document.querySelector('#player-two-name').value;
-        document.querySelector('#display').firstElementChild.textContent = `It's now ${activePlayer.name}'s turn.'`
         e.preventDefault();
     });
 
-    function updateDisplay() {
-        document.querySelector('#display').firstElementChild.textContent = `It's now ${activePlayer.name}'s turn.'`
-    };
-       
     let activePlayer = playerOne;
-    function turn() {
-        if (activePlayer == playerOne) {
-            activePlayer = playerTwo;
-        }
-        else {
-            activePlayer = playerOne;
-        }
-    };
-    function checkWin() {
-        const winCombo = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
-        for (let i = 0; i < 8; i++) {
-            const combo = winCombo[i];
-            let one = gameBoard.gameBoardContent[combo[0]];
-            let two = gameBoard.gameBoardContent[combo[1]];
-            let three = gameBoard.gameBoardContent[combo[2]];
-            if ((one === '') || (two === '') || (three === '')) {
-                continue;
-            } 
-            else if (one == two && one == three) {
-                document.querySelector('#display').firstElementChild.textContent = `${activePlayer.name} WINS!`;
-                winStatus = true;
-                document.querySelector('button').style.display = 'inline-block';
-            }
-            else if (!gameBoard.gameBoardContent.includes('') && (one != two && one != three)) {
-                document.querySelector('#display').firstElementChild.textContent =`IT's A TIE BETWEEN ${playerOne.name} and ${playerTwo.name}!`;
-                winStatus = true;
-                document.querySelector('button').style.display = 'inline-block';
-                break;
-            }
-        }
-    };
-    function addListeners() {
-        document.querySelectorAll('#cell').forEach(e => {
-            e.addEventListener('click', function() {
-                if (activePlayer.key && !e.getAttribute('data-changed', true)) {
-                    e.firstElementChild.textContent = activePlayer.key;
-                    e.setAttribute('data-changed', true);
-                    gameBoard.gameBoardContent.splice(e.getAttribute('data-element'), 1, e.firstElementChild.textContent);
-                } else if (e.getAttribute('data-changed', true)) {
-                    alert("STOP! YOU VIOLATED THE LAW!");
+            function turn() {
+                if (activePlayer == playerOne) {
+                    activePlayer = playerTwo;
                 }
-                checkWin();
-                if (!winStatus) {
-                    turn();
-                    updateDisplay();
+                else {
+                    activePlayer = playerOne;
                 }
-                });
+            };
+
+    document.querySelector('#start').addEventListener('click', e => {
+        updateDisplay();
+
+            const buttonReset = document.querySelector('#clear');
+            buttonReset.addEventListener('click', e => {
+                gameBoard.clear();
+                renderBoard.clearBoard();
+                renderBoard.newBoard();
+                document.querySelector('#display').firstElementChild.textContent = '';
+                playerOne.name = document.querySelector('#player-one-name').value = '';
+                playerTwo.name = document.querySelector('#player-two-name').value = '';
+                addListeners();
+                e.target.style.display = 'none';
             });
-    };
-    addListeners();
-    return {checkWin, addListeners,turn};
+
+            function updateDisplay() {
+                document.querySelector('#display').firstElementChild.textContent = `It's now ${activePlayer.name}'s turn.'`
+            };
+            
+            function checkWin() {
+                const winCombo = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+                for (let i = 0; i < 8; i++) {
+                    const combo = winCombo[i];
+                    let one = gameBoard.gameBoardContent[combo[0]];
+                    let two = gameBoard.gameBoardContent[combo[1]];
+                    let three = gameBoard.gameBoardContent[combo[2]];
+                    if ((one === '') || (two === '') || (three === '')) {
+                        continue;
+                    } 
+                    else if (one == two && one == three) {
+                        document.querySelector('#display').firstElementChild.textContent = `${activePlayer.name} WINS!`;
+                        winStatus = true;
+                        document.querySelector('#clear').style.display = 'inline-block';
+                    }
+                    else if (!gameBoard.gameBoardContent.includes('') && (one != two && one != three)) {
+                        document.querySelector('#display').firstElementChild.textContent =`IT's A TIE BETWEEN ${playerOne.name} and ${playerTwo.name}!`;
+                        winStatus = true;
+                        document.querySelector('#clear').style.display = 'inline-block';
+                        break;
+                    }
+                }
+            };
+            function addListeners() {
+                document.querySelectorAll('#cell').forEach(e => {
+                    e.addEventListener('click', function() {
+                        if (activePlayer.key && !e.getAttribute('data-changed', true)) {
+                            e.firstElementChild.textContent = activePlayer.key;
+                            e.setAttribute('data-changed', true);
+                            gameBoard.gameBoardContent.splice(e.getAttribute('data-element'), 1, e.firstElementChild.textContent);
+                        } else if (e.getAttribute('data-changed', true)) {
+                            alert("STOP! YOU VIOLATED THE LAW!");
+                        }
+                        checkWin();
+                        if (!winStatus) {
+                            turn();
+                            updateDisplay();
+                        }
+                        });
+                    });
+            };
+            addListeners();
+        });
+            
+        return {playerOne, playerTwo};
 })();
